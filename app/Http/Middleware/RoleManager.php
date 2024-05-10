71ef0f718a7612150ea58de085210ea2a6780b05
Hoa <damvanhoa30052002@gmail.com>
2024-05-11 01:02:26 +0700

    UI_Manager_Dashboard

---
+++ app/Http/Middleware/RoleManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Customer;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::check()){
+            $id_account = Auth::id();
+            $customer = Customer::where('id_account', $id_account)->first();
+
+            $id_user = $customer->id_user;
+
+            $user = User::where('id', $id_user)->first();
+
+            $role = $user->role;
+
+            if ($role == 'manager') {
+                return $next($request);
+            } else {
+                Auth::logout();
+                return redirect('/login');
+            }
+        }else{
+            return redirect('/login');
+        }
+    }
+}
